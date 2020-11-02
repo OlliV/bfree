8514459bd35dfdbc930c4efc5f9dad979cff8abb
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-11-02 23:04:03 +0100

    Working on workout creator

--- bfree/components/WorkoutPreview.tsx
+++ bfree/components/WorkoutPreview.tsx
@@ -1,8 +1,46 @@
 import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { useState, useEffect } from 'react';
+import { timeUnitConv, speedUnitConv, distanceUnitConv } from '../lib/units';
 import createWorkoutRunner from '../lib/workout_runner';
+import Graph, { Series } from './record/Graph';
+import { useGlobalState } from '../lib/global';
+
+const graphColors = [
+	'#ffaeae', // heart_rate
+	'#b1e67b', // power
+	'#57baeb'  // speed
+];
+
+const series: Series = [
+	{
+		id: 'abc',
+		data: [
+			{
+				x: 0,
+				y: 0,
+			},
+			{
+				x: 1,
+				y: 1,
+			},
+			{
+				x: 2,
+				y: 2,
+			},
+			{
+				x: 3,
+				y: 3,
+			},
+			{
+				x: 4,
+				y: 4,
+			},
+		],
+	}
+];
 
 function getModalStyle() {
 	const top = 50;
@@ -20,7 +58,7 @@ const useModalStyles = makeStyles((theme: Theme) =>
 		paper: {
 			position: 'absolute',
 			width: '80vw',
-			height: '45vh',
+			height: '68vh',
 			backgroundColor: theme.palette.background.paper,
 			border: '2px solid #000',
 			boxShadow: theme.shadows[5],
@@ -33,6 +71,56 @@ const useModalStyles = makeStyles((theme: Theme) =>
 		},
 	})
 );
+
+function PreviewParams() {
+	const speedUnit = speedUnitConv[useGlobalState('unitSpeed')[0]];
+	const distanceUnit = distanceUnitConv[useGlobalState('unitDistance')[0]];
+	const [endTime, setEndTime] = useState<number>(0);
+	const [endDistance, setEndDistance] = useState<number>(0);
+	const [speed, setSpeed] = useState<number>(0);
+	const [prev, setPrev] = useState<string>();
+
+	const handleEndTimeChange = (e) => {
+		const time: number = Number(e.target.value);
+
+		setEndTime(time);
+		if (prev === 'distance') {
+			setSpeed(speedUnit.convTo(distanceUnit.convToBase(endDistance) / (time * 60)));
+		} else if (prev === 'speed') {
+			setEndDistance(distanceUnit.convTo(distanceUnit.convToBase(speed) * (time * 60)));
+		}
+	};
+
+	const handleEndDistanceChange = (e) => {
+		const distance: number = Number(e.target.value);
+
+		setEndDistance(distance);
+		if (prev === 'time') {
+			setSpeed(speedUnit.convTo(distanceUnit.convToBase(distance) / (endTime * 60)));
+		} else if (prev === 'speed') {
+			setEndTime((distanceUnit.convToBase(distance) / speedUnit.convToBase(speed)) / 60);
+		}
+	};
+
+	const handleSpeedChange = (e) => {
+		const speed: number = Number(e.target.value);
+
+		setSpeed(speed);
+		if (prev === 'time') {
+			setEndDistance(distanceUnit.convTo(speedUnit.convToBase(speed) * (endTime * 60)));
+		} else if (prev === 'distance') {
+			setEndTime((distanceUnit.convToBase(endDistance) / speedUnit.convToBase(speed)) / 60);
+		}
+	};
+
+	return (
+		<form noValidate autoComplete="off">
+			<TextField value={endTime.toFixed(2)} onChange={handleEndTimeChange} onBlur={() => setPrev('time')} id="outlined-basic" label="End Time [min]" variant="outlined" />
+			<TextField value={endDistance.toFixed(2)} onChange={handleEndDistanceChange} onBlur={() => setPrev('distance')} id="outlined-basic" label="End Distance [km]" variant="outlined" />
+			<TextField value={speed.toFixed(2)} onChange={handleSpeedChange} onBlur={() => setPrev('speed')} id="outlined-basic" label="Speed [km/h]" variant="outlined" />
+		</form>
+	);
+}
 
 export default function WorkoutPreviewModal({ code, open, onClose }) {
 	const classes = useModalStyles();
@@ -64,7 +152,9 @@ export default function WorkoutPreviewModal({ code, open, onClose }) {
 	const body = (
 		<div style={modalStyle} className={classes.paper}>
 			<h2 id="workout-preview-modal-title">Workout Preview</h2>
-			<p id="workout-preview-modal-description">Lorem ipsum.</p>
+			<p id="workout-preview-modal-description">Adjust the parameters below to see how they affect to the workout.</p>
+			<PreviewParams />
+			<Graph series={series} colors={graphColors}/>
 		</div>
 	);
 
--- bfree/lib/units.ts
+++ bfree/lib/units.ts
@@ -1,18 +1,44 @@
 export interface UnitConv {
 	[index: string]: {
 		name: string;
-		convTo: (v: number) => number
+		convTo: (v: number) => number;
+		convToBase: (v: number) => number;
 	}
 }
+
+export const timeUnitConv: UnitConv = {
+	ms: {
+		name: 'millisecond',
+		convTo: (v) => v * 1000,
+		convToBase: (v) => v / 1000,
+	},
+	sec: {
+		name: 'second',
+		convTo: (v) => v,
+		convToBase: (v) => v,
+	},
+	min: {
+		name: 'minute',
+		convTo: (v) => v / 60,
+		convToBase: (v) => v * 60,
+	},
+	hour: {
+		name: 'hour',
+		convTo: (v) => v / 3600,
+		convToBase: (v) => v * 3600,
+	}
+};
 
 export const speedUnitConv: UnitConv = {
 	kmph: {
 		name: 'km/h',
 		convTo: (v) => v * 3.6,
+		convToBase: (v) => v / 3.6, // to m/s
 	},
 	mph: {
 		name: 'mph',
 		convTo: (v) => v * 2.237,
+		convToBase: (v) => v / 2.237, // to m/s
 	},
 };
 
@@ -20,13 +46,16 @@ export const distanceUnitConv: UnitConv = {
 	km: {
 		name: 'km',
 		convTo: (d) => d / 1000,
+		convToBase: (d) => d * 1000, // to m
 	},
 	m: {
 		name: 'm',
 		convTo: (d) => d,
+		convToBase: (d) => d,
 	},
 	mi: {
 		name: 'mi',
 		convTo: (d) => d * 0.000621,
+		convToBase: (d) => d * 1609, // to m
 	},
 }
