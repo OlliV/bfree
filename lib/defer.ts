08ea7c0168492e7cdb0e0e4a0395692385890c4f
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-10-26 17:03:27 +0200

    Fix some typing

--- lib/defer.ts
+++ lib/defer.ts
@@ -1,16 +1,18 @@
-export default function defer(): Promise<any> & { resolve: (v: any) => void; reject: (err: Error) => void } {
+class Defer<T> extends Promise<T> {
+	resolve: (value: unknown) => void;
+	reject: (reason?: any) => void;
+}
+
+export default function defer<T>(): Defer<T> {
 	let res;
 	let rej;
-	const promise = new Promise((resolve, reject) => {
+	const promise = new Defer<T>((resolve, reject) => {
 		res = resolve;
 		rej = reject;
 	});
 
-	// @ts-ignore
 	promise.resolve = res;
-	// @ts-ignore
 	promise.reject = rej;
 
-	// @ts-ignore
 	return promise;
 }
