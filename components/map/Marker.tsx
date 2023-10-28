2ffd5eae0fc0c7b2c4678bffe4a37e6113961217
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-10-29 01:07:38 +0200

    Export Icon

--- components/map/Marker.tsx
+++ components/map/Marker.tsx
@@ -5,22 +5,24 @@ import MarkerIcon from '../../node_modules/leaflet/dist/images/marker-icon.png';
 import MarkerShadow from '../../node_modules/leaflet/dist/images/marker-shadow.png';
 import { ReactNode } from 'react';
 
+export function createMarkerIcon() {
+	return new L.Icon({
+		iconUrl: MarkerIcon.src,
+		iconRetinaUrl: MarkerIcon.src,
+		iconSize: [25, 41],
+		iconAnchor: [12.5, 41],
+		popupAnchor: [0, -41],
+		shadowUrl: MarkerShadow.src,
+		shadowSize: [41, 41],
+	});
+}
+
 export default function MapMarker({ position, children }: { position: [number, number]; children?: ReactNode }) {
 	return (
 		<Marker
 			position={position}
 			// @ts-ignore
-			icon={
-				new L.Icon({
-					iconUrl: MarkerIcon.src,
-					iconRetinaUrl: MarkerIcon.src,
-					iconSize: [25, 41],
-					iconAnchor: [12.5, 41],
-					popupAnchor: [0, -41],
-					shadowUrl: MarkerShadow.src,
-					shadowSize: [41, 41],
-				})
-			}
+			icon={createMarkerIcon()}
 		>
 			<Popup>{children}</Popup>
 		</Marker>
