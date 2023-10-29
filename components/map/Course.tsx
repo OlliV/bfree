9c364677b4e4fe4556e12eebf84e6785e151c3a9
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-10-29 15:17:10 +0100

    Add support for multiple segments

--- components/map/Course.tsx
+++ components/map/Course.tsx
@@ -2,29 +2,47 @@ import { useEffect, useMemo } from 'react';
 import { useMap, CircleMarker } from 'react-leaflet';
 import AntPath from '../../components/map/AntPath';
 import MapWaypoint from './Waypoint';
-import { CourseData } from '../../lib/gpx_parser';
+import { CourseData, Segment } from '../../lib/gpx_parser';
 
-export default function MapCourse({ course }: { course: CourseData }) {
-	const map = useMap();
-	const { trackpoints } = course?.tracks[0]?.segments[0] || { trackpoints: [] };
-	const waypoints = course?.waypoints || [];
-	const polyline = useMemo<[number, number][]>(() => trackpoints.map(({ lat, lon }) => [lat, lon]), [trackpoints]);
+function trackSegmentsToPolylines(segments: Segment[]) {
+	return segments.map((seg) => seg.trackpoints.map(({ lat, lon }) => [lat, lon]));
+}
+
+function Segment({polyline}) {
 	const first = polyline[0];
 	const last = polyline[polyline.length - 1];
 
+	if (!first || !last) {
+		return null;
+	}
+
+	return (<>
+		{/* @ts-ignore*/}
+		<CircleMarker center={first} radius={20} pathOptions={{ color: 'blue' }} />
+		{/* @ts-ignore*/}
+		<CircleMarker center={last} radius={20} pathOptions={{ color: 'red' }} />
+		<AntPath positions={polyline} options={{ hardwareAccelerated: true, delay: 2000 }} />
+	</>);
+}
+
+export default function MapCourse({ course }: { course: CourseData }) {
+	const map = useMap();
+	const waypoints = course?.waypoints || [];
+	const polylines = useMemo(() => trackSegmentsToPolylines(course?.tracks[0]?.segments || []), [course]);
+
 	useEffect(() => {
-		if (map && polyline.length > 0) {
-			map.flyTo(polyline[0], map.getZoom());
+		if (map && polylines.length > 0 && polylines[0].length > 0) {
+			map.flyTo(polylines[0][0], map.getZoom());
 		}
-	}, [map, polyline]);
+	}, [map, polylines]);
 
 	return (
 		<>
-			{/* @ts-ignore*/}
-			{first ? <CircleMarker center={first} radius={20} pathOptions={{ color: 'blue' }} /> : null}
-			{/* @ts-ignore */}
-			{last ? <CircleMarker center={last} radius={20} pathOptions={{ color: 'red' }} /> : null}
-			<AntPath positions={polyline} options={{ hardwareAccelerated: true, delay: 2000 }} />
+			<>
+				{polylines.map((polyline, i: number) => (
+					<Segment key={i} polyline={polyline} />
+				))}
+			</>
 			<>
 				{waypoints.map(({ lat, lon, name }, i: number) => (
 					<MapWaypoint key={i} position={[lat, lon]}>
